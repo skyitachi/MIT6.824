0fc6226a31c722cd6e1ad4abead0a3b5599f9c6a
马少楠 <mashaonan@mashaonandeMBP.lan>
2019-04-13 20:59:54 +0800

    rollback to the previous version(1500 loops with one log too long)

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -57,7 +57,6 @@ func (ck *Clerk) Get(key string) string {
 	ck.seq++
 	i := ck.currentLeader
 	for {
-		retry_num := 0
 		reply := GetReply{}
 		//fmt.Println("send rpc to ", i)
 		//ck.rpcfail++
@@ -70,11 +69,7 @@ func (ck *Clerk) Get(key string) string {
 			} else if reply.Err == ErrNoKey{
 				return ""
 			} else if reply.Err == ErrTimeout{
-				time.Sleep(time.Duration(5) * time.Millisecond)
-				retry_num++
-				if retry_num <= 5 {
-					continue
-				}
+				continue
 			}
 		}
 		//if !ok {
@@ -116,7 +111,6 @@ func (ck *Clerk) PutAppend(key string, value string, op string) {
 			if reply.Err == OK {
 				return
 			} else if reply.Err == ErrTimeout {
-				time.Sleep(time.Duration(10) * time.Millisecond)
 				continue
 			}
 		}
